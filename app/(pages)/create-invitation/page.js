a9d12c95d4e12b8b71e4bafafe32ec910733270a
avrahamvais123 <avrahamvais123@gmail.com>
2024-09-09 16:59:37 +0300

    refactor: Update Progression component import in create-invitation page

--- app/(pages)/create-invitation/page.js
+++ app/(pages)/create-invitation/page.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Progression from "./components/Progression";
+import Progression from "./components/Progression.jsx";
 
 const Page = () => {
   return <Progression />;
