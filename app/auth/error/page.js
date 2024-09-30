bfd1b3efba1a1394d6e5309ff8e72736f826779e
avrahamvais123 <avrahamvais123@gmail.com>
2024-09-30 22:51:08 +0300

    refactor: Update TableHeader component to include additional WhatsApp message

--- app/auth/error/page.js
+++ app/auth/error/page.js
@@ -3,6 +3,7 @@
 import { AlertCircleIcon } from "@/app/icons/icons";
 import { cn } from "@/lib/utils";
 import { useRouter, useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
 export default function AuthErrorPage() {
   const router = useRouter();
@@ -21,25 +22,27 @@ export default function AuthErrorPage() {
   }
 
   return (
-    <div className="size-full flex-col-center justify-start pt-10 bg-slate-100">
-      <div className="w-full md:w-fit transition-all flex-col-center bg-white gap-2 py-8 md:px-16 rounded-md">
-        <AlertCircleIcon className="text-red-600 size-14" />
+    <Suspense>
+      <div className="size-full flex-col-center justify-start pt-10 bg-slate-100">
+        <div className="w-full md:w-fit transition-all flex-col-center bg-white gap-2 py-8 md:px-16 rounded-md">
+          <AlertCircleIcon className="text-red-600 size-14" />
 
-        <span className="flex-col-center">
-          <h1 className="text-4xl font-bold">שגיאה</h1>
-          <p>{errorMessage}</p>
-        </span>
+          <span className="flex-col-center">
+            <h1 className="text-4xl font-bold">שגיאה</h1>
+            <p>{errorMessage}</p>
+          </span>
 
-        <button
-          onClick={() => router.push("/auth/signup")}
-          className={cn(
-            "bg-indigo-600 hover:bg-indigo-700 active:bg-indigo-800",
-            "text-white px-4 py-2 rounded-sm mt-3"
-          )}
-        >
-          עבור לעמוד ההרשמה
-        </button>
+          <button
+            onClick={() => router.push("/auth/signup")}
+            className={cn(
+              "bg-indigo-600 hover:bg-indigo-700 active:bg-indigo-800",
+              "text-white px-4 py-2 rounded-sm mt-3"
+            )}
+          >
+            עבור לעמוד ההרשמה
+          </button>
+        </div>
       </div>
-    </div>
+    </Suspense>
   );
 }
--- app/auth/signin/page.js
+++ app/auth/signin/page.js
@@ -7,7 +7,7 @@ import { userAtom } from "@/lib/jotai";
 import { useSetAtom } from "jotai";
 import { signIn, useSession } from "next-auth/react";
 import { useRouter, useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 
 const fields = [
@@ -72,81 +72,85 @@ export default function SignIn() {
   }, [session]);
 
   return (
-    <div className="size-full flex-col-center bg-slate-50">
-      <div
-        className={cn(
-          "size-full flex-col-center gap-2",
-          "md:h-fit md:max-w-96",
-          "bg-white rounded-sm p-4"
-        )}
-      >
-        <h2 className="text-2xl text-center text-slate-400 font-bold">כניסה</h2>
-        <span className="text-xs text-center text-slate-400">
-          משתמש שנרשם דרך גוגל יש להיכנס רק דרך גוגל ומי שנרשם ידנית עם מייל
-          וסיסמה צריך להיכנס גם כן רק בדרך זו
-        </span>
-
-        <MyForm
-          title="כניסה"
-          form={form}
-          fields={fields}
-          onSubmit={onSubmit}
-          submitName="כניסה"
-          classNames={{
-            form: "pt-4",
-            fields: "rounded-sm",
-          }}
-          customSubmit={
-            <button
-              type="submit"
-              className="w-full p-2 mt-5 text-lg bg-indigo-600 text-indigo-50 rounded-sm"
-            >
-              כניסה
-            </button>
-          }
-          success={success}
-          error={error}
+    <Suspense>
+      <div className="size-full flex-col-center bg-slate-50">
+        <div
+          className={cn(
+            "size-full flex-col-center gap-2",
+            "md:h-fit md:max-w-96",
+            "bg-white rounded-sm p-4"
+          )}
         >
-          <div className="relative h- w-full h-[1px] my-4 bg-slate-200">
-            <span className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white px-2 text-sm text-gray-500">
-              או
-            </span>
-          </div>
+          <h2 className="text-2xl text-center text-slate-400 font-bold">
+            כניסה
+          </h2>
+          <span className="text-xs text-center text-slate-400">
+            משתמש שנרשם דרך גוגל יש להיכנס רק דרך גוגל ומי שנרשם ידנית עם מייל
+            וסיסמה צריך להיכנס גם כן רק בדרך זו
+          </span>
 
-          {/* google button */}
-          <button
-            type="button"
-            className="w-full rounded-sm px-4 py-2 border text-slate-400 flex justify-center items-center gap-2"
-            onClick={async () =>
-              signIn("google", {
-                callbackUrl: "/rsvp",
-              })
-            }
-          >
-            <img
-              src="/icons/google-icon.png"
-              alt="Google Icon"
-              width={20}
-              height={20}
-            />
-            כניסה עם גוגל
-          </button>
-        </MyForm>
-
-        {/* להרשמה */}
-        <span className="text-xs text-center text-slate-400">
-          עדיין לא רשומים לחצו
-          <button
-            type="button"
-            className="mr-1 text-indigo-600"
-            onClick={() => {
-              router.push("/auth/signup");
+          <MyForm
+            title="כניסה"
+            form={form}
+            fields={fields}
+            onSubmit={onSubmit}
+            submitName="כניסה"
+            classNames={{
+              form: "pt-4",
+              fields: "rounded-sm",
             }}
+            customSubmit={
+              <button
+                type="submit"
+                className="w-full p-2 mt-5 text-lg bg-indigo-600 text-indigo-50 rounded-sm"
+              >
+                כניסה
+              </button>
+            }
+            success={success}
+            error={error}
           >
-            כאן
-          </button>
-        </span>
+            <div className="relative h- w-full h-[1px] my-4 bg-slate-200">
+              <span className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white px-2 text-sm text-gray-500">
+                או
+              </span>
+            </div>
+
+            {/* google button */}
+            <button
+              type="button"
+              className="w-full rounded-sm px-4 py-2 border text-slate-400 flex justify-center items-center gap-2"
+              onClick={async () =>
+                signIn("google", {
+                  callbackUrl: "/rsvp",
+                })
+              }
+            >
+              <img
+                src="/icons/google-icon.png"
+                alt="Google Icon"
+                width={20}
+                height={20}
+              />
+              כניסה עם גוגל
+            </button>
+          </MyForm>
+
+          {/* להרשמה */}
+          <span className="text-xs text-center text-slate-400">
+            עדיין לא רשומים לחצו
+            <button
+              type="button"
+              className="mr-1 text-indigo-600"
+              onClick={() => {
+                router.push("/auth/signup");
+              }}
+            >
+              כאן
+            </button>
+          </span>
+        </div>
       </div>
-    </div>
+    </Suspense>
   );
 }
