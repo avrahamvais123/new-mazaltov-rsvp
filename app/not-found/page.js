f8a3e8dfd1deaec7c90465feb86bd50636c252e2
avrahamvais123 <avrahamvais123@gmail.com>
2024-10-08 19:39:33 +0300

    refactor: Update npm dependencies for @heroicons/react and @hugeicons/react
    Update npm dependencies for @heroicons/react to version 2.1.5 and @hugeicons/react to version 0.6.2. This ensures that the latest stable versions of these packages are used in the application.

--- app/not-found/page.js
+++ app/not-found/page.js
@@ -1,7 +1,25 @@
+import { cn } from "@/lib/utils";
+import Link from "next/link";
 import React from "react";
 
 const Page = () => {
-  return <div>not found</div>;
+  return (
+    <div className="size-full flex-col-center">
+      <h1 className="text-8xl text-center font-bold">404</h1>
+      <p className="text-center">נראה שהעמוד שחיפשת אינו נמצא</p>
+      <Link
+        href="/"
+        className={cn(
+          "py-2 px-4 mt-10",
+          "rounded-md transition-all",
+          "bg-indigo-600 hover:bg-indigo-700",
+          "text-indigo-50 text-sm font-medium"
+        )}
+      >
+        חזרה לעמוד הבית
+      </Link>
+    </div>
+  );
 };
 
 export default Page;
--- app/page.js
+++ app/page.js
@@ -1,5 +1,4 @@
 import { cn } from "@/lib/utils";
-import { Target03Icon } from "@hugeicons/react";
 import Image from "next/image";
 import CallToAction from "./components/CallToAction";
 
