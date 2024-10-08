4d381169a7076f369a09ab3b344f6392ee6527fc
avrahamvais123 <avrahamvais123@gmail.com>
2024-10-08 20:44:53 +0300

    feat: Add lazy loading for 404 page image
    Add lazy loading functionality to the 404 page image to improve page load performance. The image will now be loaded only when it is about to be displayed, reducing the initial page load time.
    
    Closes #N/A

--- app/not-found/page.js
+++ app/not-found/page.js
@@ -1,12 +1,20 @@
 import { cn } from "@/lib/utils";
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
 const Page = () => {
   return (
-    <div className="size-full flex-col-center">
-      <h1 className="text-8xl text-center font-bold">404</h1>
-      <p className="text-center">נראה שהעמוד שחיפשת אינו נמצא</p>
+    <div className="size-full flex-col-center p-6">
+        <Image
+          src="/images/not-found.png"
+          alt="תמונה של 404"
+          width={400}
+          height={300}
+          className="object-cover"
+          priority
+        />
+
       <Link
         href="/"
         className={cn(
