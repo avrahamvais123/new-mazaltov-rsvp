be9ecb362004e33d278d734d6cee150b9ebb7f97
avrahamvais123 <avrahamvais123@gmail.com>
2024-10-17 22:50:51 +0300

    refactor: Remove commented code and update Content component
    Remove the commented code block in the Content component that was responsible for reading navigation data from cookies. Also, update the Content component to include a heading and an image, improving the visual presentation of the page.
    
    Closes #N/A

--- app/(pages)/log/components/Content.jsx
+++ app/(pages)/log/components/Content.jsx
@@ -6,14 +6,6 @@ import Cookies from "js-cookie";
 export default function Content() {
   const [navigationData, setNavigationData] = useState(null);
 
-  /* useEffect(() => {
-    // קריאת העוגיה מהדפדפן
-    const data = Cookies.get("navigationData");
-    if (data) {
-      setNavigationData(JSON.parse(data));
-    }
-  }, []); */
-
   const showResults = () => {
     const data = Cookies.get("navigationData");
     if (data) {
@@ -22,8 +14,10 @@ export default function Content() {
   };
 
   return (
-    <div className="size-full flex-col-center justify-start p-4">
-      <button onClick={showResults} className="">
+    <div className="size-full flex-col-center justify-start gap-4 p-4">
+      <h1 className="text-6xl font-bold">למה לקנות כשאין לנו שקל?? למה???</h1>
+      <img src="https://img.freepik.com/premium-photo/sad-looking-person-with-sad-face-his-head_1083198-4218.jpg?w=740" className="size-96" />
+      {/* <button onClick={showResults} className="">
         show results
       </button>
       <h1>Navigation Data from Cookies</h1>
@@ -32,15 +26,30 @@ export default function Content() {
           <p>
             <strong>כותרת העמוד:</strong> {navigationData.title}
           </p>
-          <p>
-            <strong>כתובת:</strong> <a className="truncate w-10" href={navigationData.url}>{navigationData.url}</a>
+          <p className="flex-center gap-2">
+            <strong>כתובת:</strong>{" "}
+            <a
+              className="block max-w-[14rem] overflow-hidden text-ellipsis whitespace-nowrap"
+              href={navigationData.url}
+              title={navigationData.url} // מציג את ה-URL המלא ב-tooltip
+            >
+              {navigationData.url}
+            </a>
           </p>
-
-          <p className="truncate w-10">sdhjfgjhsdgfhjsagdhjfgsdhjfghjsdgfjgsdjhagfhjgsdhjgfjjfsdhjfghjsgdhjfgsdhjgfjhsgdhjafghjsdagfhjsdgahjfghjsdagfhjgsdahjf</p>
         </div>
       ) : (
         <p>No data found</p>
-      )}
+      )} */}
     </div>
   );
+}
+
+{
+  /* useEffect(() => {
+    // קריאת העוגיה מהדפדפן
+    const data = Cookies.get("navigationData");
+    if (data) {
+      setNavigationData(JSON.parse(data));
+    }
+  }, []); */
 }
